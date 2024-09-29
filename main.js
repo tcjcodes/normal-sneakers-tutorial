a722e28a5fe1488123f4956dc6ab4ba9480bde73
Trina <tcjcastillo@gmail.com>
2024-09-29 15:37:15 -0700

    feat: adjust lighting to camera and add shape controls
    1:00:43

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ import './style.css';
 import { animate, inView } from 'motion';
 import {
     AmbientLight,
-    BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, MeshLambertMaterial,
+    BoxGeometry, DirectionalLight, Group, Mesh, MeshBasicMaterial, MeshLambertMaterial,
     PerspectiveCamera, Scene, TorusKnotGeometry, WebGLRenderer,
 } from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const sneakerTag = document.querySelector('section.sneaker');
 
@@ -54,34 +55,52 @@ sneakerTag.appendChild(renderer.domElement);
 const webGLWhite = 0xffffff;
 
 const ambience = new AmbientLight(0x404040);
-scene.add(ambience);
+camera.add(ambience);
 
 const strength = 1;
 const keyLight = new DirectionalLight(webGLWhite, strength);
 keyLight.position.set(-1, 1, 3);
-scene.add(keyLight);
+camera.add(keyLight);
 
 const fillLight = new DirectionalLight(webGLWhite, 0.5);
 fillLight.position.set(1, 1, 3);
-scene.add(fillLight);
+camera.add(fillLight);
 
 const backLight = new DirectionalLight(webGLWhite, 1);
 backLight.position.set(-1, 3, -1);
-scene.add(backLight);
+camera.add(backLight);
+
+scene.add(camera);
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+controls.enablePan = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 2;
+controls.update();
 
 // SHAPE
 const geometry = new TorusKnotGeometry(1, 0.25, 100, 16);
 // requires lighting
 const material = new MeshLambertMaterial({ color: 0xff0000 });
+const shape = new Mesh(geometry, material);
 
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+// fade and pull in from bottom:
+const loadGroup = new Group();
+loadGroup.position.y = -10;
+loadGroup.add(shape);
+
+scene.add(loadGroup);
+
+animate((timeline) => {
+loadGroup.position.y = -10 + (10 * timeline);
+}, { duration: 2, delay: 1 })
 
 camera.position.z = 5;
 
 const render = () => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    controls.update();
 
     renderer.render(scene, camera);
 };
